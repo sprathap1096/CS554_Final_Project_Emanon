18ba5c294dd35ffbb6aa80c47a360a5fbf645614
Ming Lin <minglianglin50@gmail.com>
2022-05-01 23:30:39 -0400

    fix firebase boot up bug (#2)

--- lib/firebase/FirebaseService.ts
+++ lib/firebase/FirebaseService.ts
@@ -1,5 +1,5 @@
 import { FirebaseApp, getApp } from "firebase/app";
-
+import "./firebase.config";
 export default class FirebaseService {
   app: FirebaseApp;
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { createFirebaseApp } from "../lib/firebase/firebase.config";
+
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { AuthContextProvider } from "../lib/auth/AuthContextProvider";
+import "../lib/firebase/firebase.config";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
-
-  createFirebaseApp();
 
   return (
     <QueryClientProvider client={queryClient}>
