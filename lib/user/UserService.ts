b1dd46542665f8e9906b7a875efa8340ee64254a
Nisil Patel <nisil2000@gmail.com>
2022-05-11 15:31:37 -0400

    db CRUD functions for user
    Added Update functions for user

--- lib/user/UserService.ts
+++ lib/user/UserService.ts
@@ -32,6 +32,24 @@ class UserService extends FirebaseService {
     return setDoc(userDocRef, user);
   }
 
+  async updateName(userId: string, name: string) {
+    const userDocRef = this.getDocRef(userId);
+
+    return updateDoc(userDocRef, {name: name});
+  }
+
+  async updateAvatarUrl(userId: string, avatarUrl: string) {
+    const userDocRef = this.getDocRef(userId);
+
+    return updateDoc(userDocRef, {avatarUrl: avatarUrl});
+  }
+
+  async updateEmail(userId: string, email: string) {
+    const userDocRef = this.getDocRef(userId);
+
+    return updateDoc(userDocRef, {email: email});
+  }
+
   async createNewUser(user: FirebaseUser) {
     const userDocRef = this.getDocRef(user.uid);
 
@@ -44,6 +62,7 @@ class UserService extends FirebaseService {
 
     return setDoc(userDocRef, userDoc);
   }
+
 
   async getUser(userId: string) {
     const userDocRef = this.getDocRef(userId);
