c0566179b8851d280110c2582a0b2c961790d45d
Ming Lin <minglianglin50@gmail.com>
2022-05-15 22:35:51 -0400

    add pfp to browse

--- components/browse/BrowseListItem.tsx
+++ components/browse/BrowseListItem.tsx
@@ -1,18 +1,23 @@
 import useAuthContext from "@App/lib/auth/AuthContext";
-import AuthGuard from "@App/lib/auth/AuthGuard";
+
 import useAddCartItem from "@App/lib/cart/useAddCartItem";
 import { IListingAttributes } from "@App/lib/listings/types";
-import useFetchAllListing from "@App/lib/listings/useFetchAllListings";
+
+import StorageService from "@App/lib/storage/StorageService";
 import { IUserAttributes } from "@App/lib/user/types";
 import { Add, AddCircleRounded } from "@mui/icons-material";
 import {
+  Avatar,
   Box,
   CircularProgress,
+  Divider,
   IconButton,
   Paper,
   Typography,
 } from "@mui/material";
 import { DocumentSnapshot, QueryDocumentSnapshot } from "firebase/firestore";
+import { useCallback, useEffect, useState } from "react";
+import { useQueryClient } from "react-query";
 
 interface Props {
   listing: QueryDocumentSnapshot<IListingAttributes>;
@@ -20,7 +25,10 @@ interface Props {
 }
 export default function BrowseListItem({ listing, user }: Props) {
   const { currentUser } = useAuthContext();
+  const queryClient = useQueryClient();
   const { mutate: addCartItem, isLoading: isAdding } = useAddCartItem();
+
+  const [avatar, setAvatar] = useState<string>();
 
   const handleAddCart = () => {
     const { createdAt, ...listingAttr } = listing.data();
@@ -31,17 +39,36 @@ export default function BrowseListItem({ listing, user }: Props) {
     });
   };
 
+  const fetchAvatar = useCallback(() => {
+    StorageService.getDownloadUrlFromVideoUrlRef(user.data()!.avatarUrl).then(
+      (url) => setAvatar(url)
+    );
+  }, []);
+
+  useEffect(() => {
+    fetchAvatar();
+  }, [fetchAvatar]);
+
   return (
     <Box key={listing.id} paddingY={2}>
       <Paper>
         <Box display="flex" justifyContent={"space-between"} padding={2}>
           <Box>
-            <Typography>{"Title: " + listing.data().title}</Typography>
-            <Typography>{"Author: " + listing.data().author}</Typography>
-            <Typography>
-              {"Description: " + listing.data().description}
-            </Typography>
-            <Typography>{"Price: " + listing.data().price}</Typography>
+            <Box mb={2} display="flex" gap={2} alignItems="center">
+              {avatar && <Avatar src={avatar} />}
+              <Typography>{user.data()?.name}</Typography>
+            </Box>
+
+            <Divider />
+
+            <Box mt={2}>
+              <Typography>{"Title: " + listing.data().title}</Typography>
+              <Typography>{"Author: " + listing.data().author}</Typography>
+              <Typography>
+                {"Description: " + listing.data().description}
+              </Typography>
+              <Typography>{"Price: " + listing.data().price}</Typography>
+            </Box>
           </Box>
 
           <IconButton
--- lib/listings/useFetchAllListings.tsx
+++ lib/listings/useFetchAllListings.tsx
@@ -1,25 +1,27 @@
 import { useQuery } from "react-query";
+import StorageService from "../storage/StorageService";
 import UserService from "../user/UserService";
 import ListingService from "./ListingService";
 
 export default function useFetchAllListing() {
-  
   const fetchAllListing = async () => {
     const listings = await ListingService.fetchAllListings();
 
     const listingsUsers = await Promise.all(
       listings.docs.map((doc) =>
-        UserService.getUser({ userId: doc.ref.parent.id })
+        UserService.getUser({ userId: doc.ref.parent.parent!.id })
       )
     );
 
-    return listings.docs.map((listing) => {
+    const response = listings.docs.map((listing) => {
       const listingUser = listingsUsers.find(
-        (user) => user.id === listing.ref.parent.id
+        (user) => user.id === listing.ref.parent.parent!.id
       );
 
       return { listing, user: listingUser };
     });
+
+    return response;
   };
 
   return useQuery(["listings"], fetchAllListing, {});
